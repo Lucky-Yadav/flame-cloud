9edee8eef2a128d43135db23aefb787f65e408fd
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-11-09 06:57:02 +0530

    create manage and select

--- src/components/Manage.jsx
+++ src/components/Manage.jsx
@@ -1,81 +1,26 @@
 import React from 'react'
-import Chip from "@mui/material/Chip";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import Select from './Select';
 
 const Manage = () => {
-  const top100Films = [
-    { title: "Singin' in the Rain", year: 1952 },
-    { title: "Toy Story", year: 1995 },
-    { title: "Bicycle Thieves", year: 1948 },
-    { title: "The Kid", year: 1921 },
-    { title: "Inglourious Basterds", year: 2009 },
-    { title: "Snatch", year: 2000 },
-    { title: "3 Idiots", year: 2009 },
-    { title: "Monty Python and the Holy Grail", year: 1975 },
-  ];
-  const fixedOptions = [top100Films[6]];
-  const [value, setValue] = React.useState([...fixedOptions, top100Films[2]]);
   return (
     <div>
       <div className="access_popup">
         <h3>SOP Access</h3>
         <div className="sales">
           <h4>Sales</h4>
-          <div className="addteam">
-            <Autocomplete
-              multiple
-              id="fixed-tags-demo"
-              value={value}
-              onChange={(event, newValue) => {
-                setValue([
-                  ...fixedOptions,
-                  ...newValue.filter(
-                    (option) => fixedOptions.indexOf(option) === -1
-                  ),
-                ]);
-              }}
-              options={top100Films}
-              getOptionLabel={(option) => option.title}
-              renderTags={(tagValue, getTagProps) =>
-                tagValue.map((option, index) => (
-                  <Chip
-                    label={option.title}
-                    {...getTagProps({ index })}
-                    // disabled={fixedOptions.indexOf(option) !== -1}
-                  />
-                ))
-              }
-              style={{ width: 500 }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Fixed tag"
-                  placeholder="Favorites"
-                />
-              )}
-            />
-          </div>
+          <Select />
         </div>
         <div className="marketing">
           <h4>Marketing</h4>
+          <Select />
         </div>
         <div className="design">
           <h4>Design</h4>
+          <Select />
         </div>
       </div>
     </div>
   );
 }
-  const top100Films = [
-    { title: "Singin' in the Rain", year: 1952 },
-    { title: "Toy Story", year: 1995 },
-    { title: "Bicycle Thieves", year: 1948 },
-    { title: "The Kid", year: 1921 },
-    { title: "Inglourious Basterds", year: 2009 },
-    { title: "Snatch", year: 2000 },
-    { title: "3 Idiots", year: 2009 },
-    { title: "Monty Python and the Holy Grail", year: 1975 },
-  ];
 
 export default Manage
---
+++ src/components/Select.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Chip from "@mui/material/Chip";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+
+const Select = () => {
+  const Students = [
+    { title: "Aryan -Tanwar" },
+    { title: "Karan" },
+    { title: "Sandeep" },
+    { title: "Manoj Kumar" },
+  ];
+  const fixedOptions = [Students[0]];
+  const [value, setValue] = React.useState([...fixedOptions, Students[2]]);
+  return (
+    <div className="addteam">
+      <Autocomplete
+        multiple
+        id="fixed-tags-demo"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue([
+            ...fixedOptions,
+            ...newValue.filter((option) => fixedOptions.indexOf(option) === -1),
+          ]);
+        }}
+        options={Students}
+        getOptionLabel={(option) => option.title}
+        renderTags={(tagValue, getTagProps) =>
+          tagValue.map((option, index) => (
+            <Chip
+              label={option.title}
+              {...getTagProps({ index })}
+              // disabled={fixedOptions.indexOf(option) !== -1}
+            />
+          ))
+        }
+        style={{ width: 500 }}
+        renderInput={(params) => (
+          <TextField {...params} label="Fixed tag" placeholder="Favorites" />
+        )}
+      />
+    </div>
+  );
+}
+
+export default Select
